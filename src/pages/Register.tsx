fc896d84a81a89d0efd91aec74ced9f2dd9a8863
rafi <rafiahmedrabby282@gmail.com>
2025-08-27 16:05:27 +0600

    fiexed responsiveness

--- src/pages/Register.tsx
+++ src/pages/Register.tsx
@@ -3,8 +3,8 @@ import { RegisterForm } from "@/components/modules/authentication/RegisterForm"
 
 const Register = () => {
   return (
-    <div className="bg-muted flex min-h-svh flex-col items-center justify-center p-6 md:p-10">
-          <div className="w-full max-w-sm md:max-w-3xl">
+    <div className="bg-muted flex min-h-svh flex-col items-center justify-center p-6 md:p-10 border-4">
+          <div className="w-full max-w-sm  md:max-w-7xl mx-auto ">
             <RegisterForm/>
           </div>
         </div>
